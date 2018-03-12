7d2bfd3dce8847658b8cbd9e2f3028b312d80ce4
zouwubing <1756681646@qq.com>
2018-03-12 20:07:48 +0800

    fixed: mount and umount

--- remote/mount_krb100m_user_home.sh
+++ remote/mount_krb100m_user_home.sh
@@ -2,7 +2,7 @@
 this_script_dir=$(cd "$(dirname "$0")"; pwd)
 source $this_script_dir/env.sh
 # 用户配置该变量值挂载其他目录
-mount_src_dir=/home/$USERNAME_IN_KRB100M
-mount_dst_dir=$IP_OF_KRB100M
+mount_src_dir=/home/$USERNAME_IN_SBC
+mount_dst_dir=$IP_OF_SBC
 mkdir -p ~/$mount_dst_dir
-sshfs $USERNAME_IN_KRB100M@$IP_OF_KRB100M:$mount_src_dir ~/$mount_dst_dir
+sshfs $USERNAME_IN_SBC@$IP_OF_SBC:$mount_src_dir ~/$mount_dst_dir
